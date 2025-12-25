1d1aa4cbc97e406761dfbab3dd1ddcf1e7faa64b
kalpeshpatil752003-star <kalpeshpatil752003@gmail.com>
2025-12-25 10:41:21 +0530

    Fix TMDB API key for deployment

--- api/tmdb.js
+++ api/tmdb.js
@@ -1,4 +1,5 @@
-import { TMDB_API_KEY } from './keys.js';
+const TMDB_API_KEY = "f7919dfdb6ddf2bf5528aec022db0db9";
+
 const BASE_URL = "https://api.themoviedb.org/3";
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
